0a9fb8850af62e6094f51e018377fe90076ef151
Mica CF <mcohenfuentes@gmail.com>
2025-02-14 11:40:49 -0500

    setup

---
+++ server.js
@@ -0,0 +1,10 @@
+
+// Here is where we import modules
+// We begin by loading Express
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
