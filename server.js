098625ac63daab21bb11d40cdf0a2471947b906b
Mica CF <mcohenfuentes@gmail.com>
2025-02-14 13:25:41 -0500

    created a form

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+const dotenv = require("dotenv"); 
+dotenv.config(); 
 
-// Here is where we import modules
-// We begin by loading Express
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+
+  const Fruit = require("./models/fruit.js");
+
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+  // GET /fruits/new
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.ejs');
+  });
+
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
